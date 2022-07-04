0f61f6d51470e1799a19ee71a9accbab3f21f8d8
unknown <berkcanozbalci@gmail.com>
2022-07-04 14:41:23 +0300

    ransom note

---
+++ algorithms/ransom_note.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  let letters = {};
+
+  for (let i = 0; i < magazine.length; i++) {
+    if (!letters[magazine[i]]) {
+      letters[magazine[i]] = 1;
+    } else {
+      letters[magazine[i]]++;
+    }
+  }
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (!letters[ransomNote[i]]) {
+      return false;
+    }
+    letters[ransomNote[i]]--;
+  }
+
+  return true;
+};
